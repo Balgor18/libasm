1a78b50d402249d7e866ec40e4e898a8d9534ac0
Florian Catinaud <catinaud.florian@gmail.com>
2023-06-27 19:44:19 +0200

    #4 Finish the ft_write function in ASM.

--- includes/libasm.h
+++ includes/libasm.h
@@ -5,8 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/errno.h>
 
-size_t  ft_strlen(const char *s);
-
+size_t	ft_strlen(const char *s);
+size_t	ft_write(int fd, const void *buf, size_t nbyte);
 
 #endif
