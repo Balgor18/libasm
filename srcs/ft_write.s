9ed61497dbf537fd5c656c0783e0598b49e2cc27
Florian Catinaud <catinaud.florian@gmail.com>
2023-06-28 18:19:55 +0200

    Remove useless comment inside the ft_write file

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,16 +1,15 @@
-;
 ;    ssize_t     write(int fildes, const void *buf, size_t nbyte);
 ;    - fildes is store inside the register [RDI]
 ;    - buf is store inside the register [RSI]
 ;    - byte is store inside the register [RDX]
-;
+
 global _ft_write
 %define MACH_SYSCALL(nb)   0x2000000 | nb ; Use for mac os
 %define STDOUT 1
 %define WRITE 4
 
 _ft_write:
-	mov		rax, MACH_SYSCALL(WRITE); For for a linux system
+	mov		rax, MACH_SYSCALL(WRITE); Check for a linux system
 	mov		rbx, rdx
 	syscall
 	cmp		rax, 0
